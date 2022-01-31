02a1961e06e66a0e95ad7510802eee6df7354090
Steve Canneaux <seamlessloops@gmail.com>
2022-01-31 21:58:34 +0000

    Add files via upload

---
+++ before code fixed (homework).py
@@ -0,0 +1,14 @@
+def stringReverse(aString):
+ rString = ''
+ index = len(aString)
+ while index > 0:
+ rString += aString[ index - 1 ]
+ index = index - 1
+ print(rString)
+string1 = "Hello World"
+print("Original String =", string1)
+print("")
+print("After Reverse = ")
+stringReverse(string1)
+
+
